ff9f9a440098cabd81fb5a282796f247fef49d5e
Jake <admin@jakerunsdnd.com>
2017-12-28 15:05:52 -0700

    Initial setup for various commands

--- bot.js
+++ bot.js
@@ -7,17 +7,30 @@ const Discord = require("discord.js");
  */
 
 const client = new Discord.Client();
+const hotkey = '!'; //This determines what aggravates the bot
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (cmd.startsWith(hotkey)){
+    let pieces = msg.content.split(' ');
+    let cmd = pieces[0];
+    let input = pieces[1] || null;  //Some cmds are just the cmd
+    console.log('Recieved cmd '+cmd+' for input ['+input+']');
+    if (cmd.startsWith( hotkey + 'roll' )) {
+      msg.reply('Rolling '+ input + '...');
+      msg.reply(roll(input));
+    } else if (cmd.startsWith( hotkey + 'rank' )) {
+      msg.reply('Rank '+ input + '...');
+      msg.reply(rank(input));
+    } else if (cmd.startsWith( hotkey + 'ping' )) {
+      msg.reply('Pong!');
+    }
   }
 });
 
-console.log('Attempting login...');
+console.log('Rolling initiative: ' + d20() );
 client.login('token');
 console.log('I\'ll be back');

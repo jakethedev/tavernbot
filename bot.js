ef9a58f8c308c330f96971dc2ed08b4d533b0487
Jake <admin@jakerunsdnd.com>
2017-12-28 14:05:20 -0700

    Initial code bits for bot. Nuked python unnecessary stuff, tweaked a couple files to fit node stuff better. Still needs some conversion

--- bot.js
+++ bot.js
@@ -1,6 +1,23 @@
 require('./lib/dicelib')
+const Discord = require("discord.js");
 /**
- * require discord
+ * X require discord
  * load discord conf
  * init client
  */
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+console.log('Attempting login...');
+client.login('token');
+console.log('I\'ll be back');

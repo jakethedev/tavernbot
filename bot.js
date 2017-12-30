6b5c7e57bf0edac4b3a31f7a23506ea86c05dc0b
jake@chrx <admin@jakerunsdnd.com>
2017-12-29 23:05:47 -0700

    Lots of bot updates, runs for sure and works on a default channel and DMs. Easily configurable for now, will be easier to configure later

--- bot.js
+++ bot.js
@@ -1,46 +1,51 @@
-//All the custom stuff, loaded as global functions. i.e. d20() is now global. Like it should be.
+//All the custom stuff, loaded as global functions for pragmatism
 require('./lib/dicelib')
 require('./lib/roleslib')
 require('./lib/hooklib')
-
-/**
- *     TODO
- * X require discord
- * load discord conf
- * init client
- */
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json"); // Should contain token and botkey, a character that prefixes commands
 
+// In case something happens, we'll want to see logs
+client.on("error", (e) => console.error(e));
+
+// Startup callback
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({
+    "status":"online",
+    "game": { "name" : config.gameStatus }
+  });
 });
 
+// Command central
 client.on('message', msg => {
   // msg = { "content": "!adventure", "author": { "bot": false }, "reply": console.log }; //Test item, do not use
 
-  //Verify that we care, and that we're not making ourselves care
-  if ( msg.content.startsWith(config.botkey) && (!msg.author.bot) ){
-    //Remove botkey and break it up
-    let pieces = msg.content.substring(1).split(' ');
-    let cmd = pieces[0];
-    let input = pieces[1] || null;  //Some cmds are just the cmd
-    console.log('Recieved request to ['+cmd+'] for input ['+input+']');
+  // Let's hook it up for a default channel and DMs
+  if ( msg.channel.name == config.defaultChannel || msg.channel.recipient ){
+    //Make sure we care, and that we're not making ourselves care
+    if ( !msg.content.trim().startsWith(config.botkey) || msg.author.bot) return;
+
+    //Remove botkey and break it up into clean pieces
+    let parts = msg.content.trim().substring(1).split(/\s+/);
+    let cmd = parts[0];
+    let input = parts[1] || null;  //Some cmds are just the cmd
 
     //From here, we're just using dicelib/roleslib/etc functions
+    console.log( msg.author.username +' requested ['+cmd+'] for input ['+input+']');
     if ( cmd == 'roll' ) {
       if ( input ){
         msg.reply( customRoll(input) );
       } else {
-        msg.reply('A d20 skitters across the table, you rolled a ' + d(20) );
+        msg.reply('a d20 skitters across the table, you rolled a ' + d(20) );
       }
     } else if ( cmd == 'coin' ) {
-      msg.reply( "The botcoin landed on " + coin() );
+      msg.reply( "the botcoin landed on " + coin() );
     } else if ( cmd == 'd' && input && parseInt(input) ) {
       let diceSize = parseInt(input);
-      msg.reply( "Your custom die rolls a " + d(diceSize) );
+      msg.reply( "your custom die rolls a " + d(diceSize) );
     } else if ( cmd == 'rank' && input ) {
       msg.reply( addRole(client, msg.author, input) );
     } else if ( cmd == 'ranks' ) {
@@ -49,12 +54,12 @@ client.on('message', msg => {
       msg.reply( removeRole(client, msg.author, input) );
     } else if ( cmd == 'members' && input ) {
       msg.reply( listUsersInRole(client, input) );
-    } else if ( cmd == 'adventure' ) {
+    } else if ( cmd == 'rpghook' ) {
       msg.reply(generateAdventure(false));
     }
   }
 });
 
-console.log('Rolling initiative: ' + d(20) );
+// Turning the key and revving the bot engine
+console.log('Rolling initiative...');
 client.login(config.token);
-console.log('I\'ll be back');

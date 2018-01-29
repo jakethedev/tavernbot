2f859ca572dfd8df711913e1dc356b41f118032e
jake@chrx <admin@jakerunsdnd.com>
2018-01-28 21:21:51 -0700

    Daemon tweaks

--- daemon.sh
+++ daemon.sh
@@ -4,8 +4,8 @@ PATH="$PATH:/user/bin"
 # Check for a nodemon process, and if there isn't one, run it
 pgrep node -a || absent=true
 if [[ $absent ]]; then
-  echo "No nodemon found, summoning..."
+  echo "$(date): WARN No nodemon found, summoning..."
   npm run daemon &
 else
-  echo "Running as expected"
+  echo "$(date): OK Running as expected"
 fi

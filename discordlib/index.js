888ded1e26dec5b93e51cc1c4c46fd59b7efc618
jake <admin@jakerunsdnd.com>
2018-01-29 15:45:25 -0700

    Readme tweak.

---
+++ discordlib/index.js
@@ -0,0 +1,14 @@
+// List all local components here
+const components = [
+  './discordroles'
+]
+
+// Go through each chunk of the library and set each exported
+// function as its own export of this module
+for (sublib of components){
+  let lib = require(sublib)
+  for (operation in lib){
+    exports[operation] = lib[operation]
+  }
+  console.log( sublib + " loaded!" )
+}

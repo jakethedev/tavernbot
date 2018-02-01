ab4eef75ce6354675e461a1dc8e3eceae19491be
Jake <admin@jakerunsdnd.com>
2018-01-31 21:08:24 -0700

    Beta summoning function

--- discordlib/chatter.js
+++ discordlib/chatter.js
@@ -1,3 +1,27 @@
 exports.sass = function(input){
   return 'ain\'t nobody got time for that'
 }
+
+exports.summon = function(input, message){
+  if (message.mentions && message.mentions.users.first){
+    let targetToSummon = message.mentions.users.first
+    let response = `Throwing a salt circle...
+
+  \`\`\`           %%%    %%%
+        %%%              %%%
+
+    %%%                      %%%
+
+   %%%                         %%%
+          ${targetToSummon}
+   %%%                         %%%
+
+   %%%                        %%%
+
+      %%%                  %%%
+
+            %%%     %%%\`\`\``
+    return response
+  }
+  return 'you can\'t just summon nothing, that\'s not how this works!'
+}

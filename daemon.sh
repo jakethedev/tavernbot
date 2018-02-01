59b9f0b13dcbe79cea263c367fbf49ecac3f9df8
Jake <admin@jakerunsdnd.com>
2018-01-31 20:35:24 -0700

    Daemon update, should be last one

--- daemon.sh
+++ daemon.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 PATH="$PATH:/user/bin"
 LOCK='./daemon.lock'
+LOG='./pidaemon.log'
 
 # Check for a nodemon process, and if there isn't one, run it
 if [[ -f $LOCK ]]; then
-  echo "$(date): WARN Already summoning!"
+  echo "$(date): WARN Already summoning!" >> $LOG
 else
   touch $LOCK
   pgrep node -a || absent=true
   if [[ $absent ]]; then
-    echo "$(date): WARN No nodemon found, summoning..."
-    npm run daemon > pidaemon.log &
+    echo "$(date): WARN No nodemon found, summoning..." >> $LOG
+    npm run daemon >> $LOG &
   else
-    echo "$(date): OK Running as expected"
+    echo "$(date): OK Running as expected" >> $LOG
   fi
   rm $LOCK
 fi

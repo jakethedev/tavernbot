4c4ea6a4b3ef082a0ab80ac2449224fcf29b1cbb
jake@chrx <admin@jakerunsdnd.com>
2018-03-17 21:00:38 -0600

    Formatting tweak for #28

--- discordlib/chatter.js
+++ discordlib/chatter.js
@@ -3,29 +3,27 @@ exports.sass = function(input) {
 }
 
 exports.summon = function(input, message) {
-  //TODO Consider a ring of :fire: and actually tagging summoned user
   if (message.mentions.users) {
     if (message.mentions.users.size == 1) {
       let summoned = message.mentions.members.first()
-      // The summon circle is 32 chars wide, 16 is the mid point
+      // The summon circle is ~36 chars wide, 18 or 19 is the mid point
       let padding = ' '.repeat(18 - (summoned.displayName.length / 2))
       return `your wish is my command...
 
 COME FORTH ${summoned}! \`\`\`
-             %%%    %%%
-        %%%              %%%
+             %#%    %#%
+        %#%              %#%
 
-    %%%                      %%%
+    %#%                      %#%
 
-   %%%                         %%%
+   %#%                         %#%
 ${padding + summoned.displayName}
-   %%%                         %%%
+   %#%                         %#%
 
-   %%%                        %%%
+    %#%                      %#%
 
-      %%%                  %%%
-
-            %%%     %%%\`\`\``
+        %#%              %#%
+             %#%    %#%\`\`\``
     }
   }
   return "you can't just summon nothing, that's not how this works!"

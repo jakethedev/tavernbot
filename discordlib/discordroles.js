a4e2f7674c83b1387b76ec88022a951730f38be0
jake@chrx <16889000+jakethedev@users.noreply.github.com>
2018-10-18 18:29:56 -0600

    Roles command complete, friendlier server command output. Closes #81 - and thanks for the heads up on in #78 @j2l :)

--- discordlib/discordroles.js
+++ discordlib/discordroles.js
@@ -18,67 +18,89 @@ exports.giverole = exports.addrole = function(input, message, client) {
   if (!input.trim()) return `you can't just add nothing as a role, that's not how any of this works!`
   let expectedRoleName = input.split('<')[0].toLowerCase().trim() //Expecting only one role, before any mentions
   // Allows us to add a role to someone, pinging them required
-  let requestorName = message.member.user.username
-  let optionalMention = message.mentions.members.first()
-  let targetMember = optionalMention ? optionalMention : message.member
-  let targetName = targetMember.user.username
-  let roleToAdd = message.guild.roles.find((role) => expectedRoleName == role.name.toLowerCase())
-  if (!roleToAdd){
-    return `that role does not exist, checkest thy typing or speaketh with thy lord moderators`
+  if (message.guild) {
+    let requestorName = message.member.user.username
+    let optionalMention = message.mentions.members.first()
+    let targetMember = optionalMention ? optionalMention : message.member
+    let targetName = targetMember.user.username
+    let roleToAdd = message.guild.roles.find((role) => expectedRoleName == role.name.toLowerCase())
+    if (!roleToAdd){
+      return `that role does not exist, checkest thy typing or speaketh with thy lord moderators`
+    }
+    console.log(`Role '${roleToAdd.name}' requested by ${requestorName} for ${targetName}...`)
+    return targetMember.addRole(roleToAdd).then(result => {
+      // S'gooood. This is idempotent, adding an existing role id a-ok
+      return `${targetName} now has (or already had) the role ${roleToAdd.name}!`
+    }).catch(err => {
+      // Almost certainly a permission error
+      return `I can't add ${targetName} to ${roleToAdd.name}, probably not allowed to. Contact an admin if this is unexpected`
+    });
+  } else {
+    return `run this command on a server with roles to get a more helpful response :)`
   }
-  console.log(`Role '${roleToAdd.name}' requested by ${requestorName} for ${targetName}...`)
-  return targetMember.addRole(roleToAdd).then(result => {
-    // S'gooood. This is idempotent, adding an existing role id a-ok
-    return `${targetName} now has (or already had) the role ${roleToAdd.name}!`
-  }).catch(err => {
-    // Almost certainly a permission error
-    return `I can't add ${targetName} to ${roleToAdd.name}, probably not allowed to. Contact an admin if this is unexpected`
-  });
 }
 
 // List roles on the server that the bot can assign
 exports.roles = function(input, message, client) {
   if (input.toLowerCase() == 'help')
     return `'roles' will get you a list of the server roles that I can grant you`
-  return `the only important role is to butter toast (jake hasn't finished this yet)`
+  // If we're on a server, get them roles - reply intelligently in unhappy circumstances
+  if (message.guild) {
+    const roleList = message.guild.roles
+                        .filter(role => !role.name.includes('@everyone'))
+                        .map(role => `'${role.name}'`).join(', ')
+    if(roleList) {
+      return `a server has many roles:\n${roleList}`
+    } else {
+      return `a server has no roles, bribe a mod to add one`
+    }
+  } else {
+    return `run this command on a server with roles to get a more helpful response :)`
+  }
 }
 
 //List the requestor's roles.
 //TODO Use this, list a target's roles; let targetToSummon = message.mentions.users.first()
 exports.myroles = function(input, message, client) {
   if (input.toLowerCase() == 'help') return `'myroles' will list all roles you have here`
-  const userRolesRaw = message.member.roles
-  let roleResults = []
-  // Stash the results, strip any @ symbols to avoid pinging @everyone every single time
-  userRolesRaw.forEach(role => roleResults.push(role.name.replace('@', '')))
-  if (roleResults[0]) {
-    return `here are your roles: [${roleResults.join(', ')}]`
+  if (message.guild) {
+    const userRolesRaw = message.member.roles
+    let roleResults = []
+    // Stash the results, strip any @ symbols to avoid pinging @everyone every single time
+    userRolesRaw.forEach(role => roleResults.push(role.name.replace('@', '')))
+    if (roleResults[0]) {
+      return `here are your roles: [${roleResults.join(', ')}]`
+    } else {
+      return `your purpose is to butter toast. (no roles found)`
+    }
   } else {
-    return `your purpose is to butter toast. (no roles found)`
+    return `run this command on a server with roles to get a more helpful response :)`
   }
 }
 
 //Self-remove a role, after verifying that the author is a member (role-bearer)
 exports.unrole = function(input, message, client) {
   if (!input || input.toLowerCase() == 'help') return `'unrole rolename' will rolename from your roles if you have it`
-  if (!message.member) return `this is not the command you're looking for (you're not a member here, sorry mate)`
-  let userRoles = message.member.roles
-  if (userRoles.size == 0)
-    return `it seems that you have no roles, and that's really funny`
-  let roleResult = userRoles.find(role => role.name.toLowerCase() === input.toLowerCase())
-  return message.member.removeRole(roleResult).then(result => {
-    return `you are uninvited from ${input}`
-  }).catch(error => {
-    return `I'm afraid I can't do that, Dave. Either you don't have that role or a mod needs to handle it`
-  })
+  if (message.guild) {
+    let userRoles = message.member.roles
+    if (userRoles.size == 0)
+      return `it seems that you have no roles, and that's really funny`
+    let roleResult = userRoles.find(role => role.name.toLowerCase() === input.toLowerCase())
+    return message.member.removeRole(roleResult).then(result => {
+      return `you are uninvited from ${input}`
+    }).catch(error => {
+      return `I'm afraid I can't do that, Dave. Either you don't have that role or a mod needs to handle it`
+    })
+  } else {
+    return `run this command on a server with roles to get a more helpful response :)`
+  }
 }
 
 //Number of people in a given role
 exports.rolesize = function(input = '', message, client) {
   if (!input) return `give me a role and I'll give you an answer`
   if (input.toLowerCase() == 'help') return `'rolesize role-name' prints the size of a role - this might go away soon though`
-  if (message.guild.available) { //Docs recommend this check
-
+  if (message.guild) {
     //Make input easier to search with, comb the roles, and return the size of the role if it's found
     input = input.trim().toLowerCase()
     let roleResult = message.guild.roles.find(role => role.name.toLowerCase() === input)
@@ -89,7 +111,7 @@ exports.rolesize = function(input = '', message, client) {
       return `role not found - gimme the role's full name, and I'll get you a member count`
     }
   } else {
-    return `there was a temporal anomaly, I believe I need my oil changed`
+    return `run this command on a server with roles to get a more helpful response :)`
   }
 }
 
@@ -97,11 +119,11 @@ exports.rolesize = function(input = '', message, client) {
 exports.rolemembers = function(input = '', message, client) {
   if (!input) return `give me a role and I'll give you an answer`
   if (input.toLowerCase() == 'help') return `'rolemembers role-name' definitely doesn't list the members of a role`
-  if (message.guild.available) { //Docs recommend this check
+  if (message.guild) { //Docs recommend this check
     //TODO Copy rolesize function but list names
-    return `under construction (jake hasn't finished this yet)`
+    return `under construction (the artificer hasn't finished this yet)`
   } else {
-    return `there was a temporal anomaly, I believe I need my oil changed (rolemember guild error)`
+    return `run this command on a server with roles to get a more helpful response :)`
   }
 }
 
